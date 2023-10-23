41748dc8ec286f54d80a33c331e81331d83d305e
NazarenoCovian <nazareno.esso@hotmail.com>
2023-10-22 21:46:00 -0300

    add db port

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from "../config.js";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "../config.js";
 
 // export const sequelize = new Sequelize("projectsdb", "postgres", "mysecretpassword", {
 //     host: "localhost",
@@ -9,4 +9,5 @@ import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from "../config.js";
 export const sequelize = new Sequelize(DB_NAME,DB_USER,DB_PASSWORD,{
     host: DB_HOST,
     dialect: "postgres",
+    port: DB_PORT,
 })
